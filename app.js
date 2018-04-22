89367ce9a84a0a553b461b06e5aa45fab8ff6efd
Kunal Agarwal <kunal94agarwal@gmail.com>
2018-04-22 16:38:47 +0530

    first https request to darksky api

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
 const request = require('request');
 const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+// .options({
+//   a: {
+//     demand: true,
+//     alias: 'address',
+//     describe: 'Address to fetch weather for',
+//     string: true
+//   }
+// })
+// .help()
+//
+// .argv;
+//
+// geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+//   if(errorMessage) {
+//     console.log(errorMessage);
+//   }else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
 
-const argv = yargs
-.options({
-  a: {
-    demand: true,
-    alias: 'address',
-    describe: 'Address to fetch weather for',
-    string: true
-  }
-})
-.help()
+  // fb33040bfa183db0e7ea81a3374cfce0
 
-.argv;
-
-geocode.geocodeAddress(argv.address, (errorMessage, results) => {
-  if(errorMessage) {
-    console.log(errorMessage);
-  }else {
-    console.log(JSON.stringify(results, undefined, 2));
+request ({
+  url: 'https://api.darksky.net/forecast/fb33040bfa183db0e7ea81a3374cfce0/37.8267,-122.4233',
+  json: true
+}, (error, response, body) => {
+  if (error){
+    console.log('unable to connect to forecast servers');
+  }else if (response.statusCode === 400){
+    console.log('Unable to fetch error');
+  }else if(response.statusCode === 200){
+    console.log(body.currently.temperature);
   }
 });
