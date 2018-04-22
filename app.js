e63e68e6f09e15d26892960c49ae3dfda891bd44
Kunal Agarwal <kunal94agarwal@gmail.com>
2018-04-22 11:06:17 +0530

    error handling in request

--- app.js
+++ app.js
@@ -13,14 +13,20 @@ const argv = yargs
 .help()
 .alias('help', 'h')
 .argv;
-
+  console.log(argv);
   var encodedAddress = encodeURIComponent(argv.address);
 
 request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-  console.log(`Longitite: ${body.results[0].geometry.location.lng}`);
+  if(error){
+    console.log('Unable to connect to server');
+  }else if (body.status === 'ZERO_RESULTS') {
+    console.log('Unable to find that address.');
+  }else if (body.status === 'OK') {
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Longitite: ${body.results[0].geometry.location.lng}`);
+  }
 });
