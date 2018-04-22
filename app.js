3c16373cd9f092e399b07dee487657cae48e6d1f
Kunal Agarwal <kunal94agarwal@gmail.com>
2018-04-22 12:48:36 +0530

    creating geocode function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
+
 const argv = yargs
 .options({
   a: {
@@ -11,22 +13,7 @@ const argv = yargs
   }
 })
 .help()
-.alias('help', 'h')
-.argv;
-  console.log(argv);
-  var encodedAddress = encodeURIComponent(argv.address);
 
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-  json: true
-}, (error, response, body) => {
-  if(error){
-    console.log('Unable to connect to server');
-  }else if (body.status === 'ZERO_RESULTS') {
-    console.log('Unable to find that address.');
-  }else if (body.status === 'OK') {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitite: ${body.results[0].geometry.location.lng}`);
-  }
-});
+.argv;
+
+geocode.geocodeAddress(argv.address);
